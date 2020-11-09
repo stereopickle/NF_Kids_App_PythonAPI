ab0e5692f954291ecb9c22727f17d0237653a4de
stereopickle <stereopickle@gmail.com>
2020-11-08 19:43:41 -0500

    Updating descriptions

--- api/api.py
+++ api/api.py
@@ -1,9 +1,39 @@
-import flask as fl
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: stereopickle / Eunjoo Byeon
+This script contains Python API
+"""
 
-app = fl.Flask(__name__)
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+
+
+# defining api
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-# test data 
+
+"""
+Multi-label classification of symptoms
+
+There are many ways NF1 can develop and create complications.
+Based on descriptions of symptoms, our goal is to provide
+possible development so parents can be aware of what to look out for.
+
+Our model will be trained as multi-label classifier on text descriptions
+of each possible development. (need to collect data)
+
+Below pipeline will do the following: 
+    1. retrieve the text log when '/completelog' is called.
+    2. Preprocess text log
+    3. Make prediction using multi-label classifier
+    4. Return probability of each label
+    
+"""
+
+
+
 
 dat = [
        {'id': 0, 
---
+++ api/nlp_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: stereopickle / Eunjoo Byeon
+This script contains multi-label classification model
+
+There are many ways NF1 can develop and create complications.
+Based on descriptions of symptoms, our goal is to provide
+possible development so parents can be aware of what to look out for.
+
+This script trains the model and return the model.
+
+"""
+
